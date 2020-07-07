bb06757675635398ee692fc130ae9855755dbdcc
Jonathan Modlin <jonathanmodlin1@gmail.com>
2020-07-07 11:42:58 -0400

    heroku work!

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <Link className="navbar-brand" to="/">
         Google Books Search
       </Link>
