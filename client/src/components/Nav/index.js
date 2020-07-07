0a0bfe87fc7737d80a7bf412f7abddc60ddf9b29
Jonathan Modlin <jonathanmodlin1@gmail.com>
2020-07-07 14:36:34 -0400

    commit

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
       <Link className="navbar-brand" to="/">
         Google Books Search
       </Link>
