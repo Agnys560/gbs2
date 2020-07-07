bd4f866846765e569d2d766a7fbcafb893699acb
Jonathan Modlin <jonathanmodlin1@gmail.com>
2020-07-07 12:03:39 -0400

    heroku work!

--- models/Book.js
+++ models/Book.js
@@ -1,23 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const BookSchema = new Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  authors: {
-    type: String
-  },
-  image: {
-    type: String
-  },
-  description: {
-    type: String
-  },
-  link: {
-    type: String,
-    required: true
-  }
+  title: { type: String, required: true },
+  authors: [{ type: String, required: true }],
+  synopsis: String,
+  date: { type: Date, default: Date.now },
+  thumbnail: { type: String },
+  link: String
 });
 
 var Book = mongoose.model("Book", BookSchema);
